439c68842fbc62c9690bff6f32bb421a88e0376b
mage1232010 <mage1232010@gmail.com>
2023-02-01 12:15:40 -0300

    Exercício Calculadora IMC

---
+++ aula12.py
@@ -0,0 +1,10 @@
+#Exercício 02 - Calcular IMC
+
+nome = 'Edison Junior'
+altura = 1.71
+peso = 58
+imc = peso / altura ** 2
+
+print(nome, 'tem', altura, 'de altura,')
+print('pesa', peso, 'quilos e seu IMC é')
+print(imc)
